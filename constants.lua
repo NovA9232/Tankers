231ba3d0fd418e07ec91e2073027b266e03f701d
12ColcloughJ <joshuacolclough2@googlemail.com>
2019-02-03 17:58:38 +0000

    Trying to get movement to work correctly.

--- constants.lua
+++ constants.lua
@@ -2,4 +2,5 @@ METER = 10  -- Size of 1 meter in pixels
 
 -- Tank:
 TANK_ACC = 0.1  -- Tank acceleration in m/s-2
-TANK_DECEL = 0.97   -- Delceleration when no keys are pressed. Should be less than 1
+TANK_DECEL = 0.98   -- Delceleration when no keys are pressed. Should be less than 1
+TANK_ROTATE_RATE = math.pi/8  -- Rotational speed of tank in radians per second.
--- tank.lua
+++ tank.lua
@@ -4,31 +4,49 @@ function Tank(id, x, y)
   t.x  = x
   t.y  = y
   t.angle = 0
-  t.vel = Vec2(0, 0)
+  t.pointOfRotation = Vec2(0, 0)  -- Offset from middle
+  t.velMag = 0 -- Magnitude of acceleration
   t.w = 30
   t.h = 50
+
+  function t:getMiddle()
+    return self.x+(self.w/2), self.y+(self.h/2)
+  end
 
   function t:draw()
     lg.setColor(1, 1, 1, 1)
     lg.push()
-      lg.translate(self.x+(self.w/2), self.y+(self.h/2))
+      midX, midY = self:getMiddle()
+      lg.translate(midX+self.pointOfRotation.x, midY+self.pointOfRotation.y)
       lg.rotate(self.angle)
+      lg.translate(-self.pointOfRotation.x, -self.pointOfRotation.y)
       lg.rectangle("line", -(self.w/2), -(self.h/2), self.w, self.h)
     lg.pop()
   end
 
+  function t:applyResistance(dt)  -- Due to friction/air resistance
+    self.velMag = self.velMag*(TANK_DECEL*math.min(1/dt, 1))
+  end
+
   function t:update(dt)
     if love.keyboard.isDown("w", "up") then
-      self.vel.y = self.vel.y - (TANK_ACC*METER)
+      self.velMag = self.velMag + (TANK_ACC*METER)*dt
+    end
+
+    if love.keyboard.isDown("a", "left") then
+      t.pointOfRotation = Vec2(self.w/2, self.h/2)
+      self.angle = self.angle + (TANK_ROTATE_RATE*dt)
+      self.velMag = self.velMag + ((TANK_ACC*METER)*dt)/2  -- /2 since only 1 track
+      t.pointOfRotation = Vec2(0, 0)
     end
 
     if not love.keyboard.isDown("w", "a", "s", "d", "up", "left", "down", "right") then
-      self.vel.x = self.vel.x*(TANK_DECEL*math.min(1/dt, 1))
-      self.vel.y = self.vel.y*(TANK_DECEL*math.min(1/dt, 1))
+      self:applyResistance(dt)
     end
 
-    self.x = self.x + self.vel.x*dt
-    self.y = self.y + self.vel.y*dt
+    self.x = self.x + self.velMag*math.cos(self.angle)
+    self.y = self.y + self.velMag*math.sin(self.angle)
+    print(self.velMag*math.cos(self.angle), self.velMag*math.sin(self.angle))
   end
 
   table.insert(entities.players, t)
